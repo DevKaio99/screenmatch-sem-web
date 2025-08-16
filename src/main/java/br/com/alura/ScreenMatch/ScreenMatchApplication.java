c5d3ca41c993d0b527abd90461a019a21c2adb64
Kaio Andrade <kaiokakarma@hotmail.com>
2025-08-15 22:46:19 -0300

    Aplicação screen match com APIs

--- src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,27 +1,24 @@
 package br.com.alura.ScreenMatch;
 
-import br.com.alura.ScreenMatch.model.DadosSerie;
-import br.com.alura.ScreenMatch.service.ConsumoAPI;
-import br.com.alura.ScreenMatch.service.ConverteDados;
+import br.com.alura.ScreenMatch.principal.Principal;
+import br.com.alura.ScreenMatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication implements CommandLineRunner {
+	@Autowired
+	private SerieRepository repositorio;
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScreenMatchApplication.class, args);
+		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=ceda4f87");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
 	}
 }
